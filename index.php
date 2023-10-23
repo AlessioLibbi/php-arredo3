cc0afcb689e9a62336eb002138ff86fe325a8667
Alessio Libbi <alex.libbi@gmail.com>
2023-10-23 14:34:45 +0200

    Add:hello world

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <nav class="container d-flex justify-content-around">
+            <div class="container-left">
+                <?php echo "<h1>Hello World</h1>" ?>
+            </div>
+            <div class="container-right">
+                <ul class="d-flex">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Contattaci</a></li>
+                    <li><a href="">Seguici</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+    
+    </header>
+    
+    
+</body>
